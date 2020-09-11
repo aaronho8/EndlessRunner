4ada7c63078b8d936902bc61f4d52d392bb4c77e
Aaron Ho <aayho@ucsc.edu>
2020-09-10 18:57:35 -0700

    initial commit

---
+++ Assets/Scripts/PlayerCollide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollide : MonoBehaviour
+
+{
+    public PlayerMovement movement;
+    public SpawnManager spawnManager;
+
+    
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle"){
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+
+        }
+
+    }
+    public void OnTriggerEnter(Collider other){
+        spawnManager.SpawnTriggerEntered();
+    }
+    
+}
